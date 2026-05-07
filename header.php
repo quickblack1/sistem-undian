f26106fb787fa0bbc7f44e4f9243a28ea5c6f87f
quickblack1 <najmeedaud@gmail.com>
2026-05-07 16:32:51 +0800

    a

--- header.php
+++ header.php
@@ -19,21 +19,21 @@
     -->
 
     <script type="text/javascript">
-    function googleTranslateElementInit() {
-      new google.translate.TranslateElement({pageLanguage: 'ms'}, 'google_translate_element');
-    }
+      function googleTranslateElementInit() {
+        new google.translate.TranslateElement({pageLanguage: 'ms'}, 'google_translate_element');
+      }
     </script>
+
     <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     <div id="display-date">
         <script language="javascript"> 
-        today = new Date();
-        var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var yr = today.getYear();
+          today = new Date();
+          var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+          var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+          var yr = today.getYear();
 
-
-        if(yr < 1900) yr += 1900;
-        document.write(today.getDate() + " " + month[today.getMonth()] + " " + yr);
+          if(yr < 1900) yr += 1900;
+          document.write(today.getDate() + " " + month[today.getMonth()] + " " + yr);
         </script>
     </div>
 
