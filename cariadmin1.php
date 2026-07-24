22ff9b92a9f5b965a154e9e9a077d9dd8fcc86c6
quickblack1 <najmeedaud@gmail.com>
2026-07-24 09:33:04 +0800

    a

--- cariadmin1.php
+++ cariadmin1.php
@@ -11,10 +11,6 @@ include("header.php");
 
 
 <?php 
-
-
-
-
 $idcalon = $_GET['idcalon'];
 
  //get the idpelajar which will updated
@@ -48,84 +44,58 @@ $kelas = $rekod['kelas'];
 
 ?>
 <form method="GET" action="proseseditadmin.php">
-<table border="0" cellpadding="2" cellspacing="2">
-  <tr bgcolor="orange">
-  <td height="21" colspan="3">
-     <strong> Kemaskini Calon</strong>
-    </td>
-
-<tr>
-    <td> gambar</td>
-    <td>:</td>
-    <td> <img src = '<?php echo $gambar; ?>' width='200px'> <br><td><a href='confirm.php? idcalon=<?php echo $idcalon; ?>'> Kemaskini gambar </a> </i></td>
-
-
-  </tr>
-
-  <tr>
-    <td> Id Calon </td>
-    <td>:</td>
-    <td> <input type="text" name="idcalon" disabled value= '<?php echo $idcalon; ?>'> </td>
-  </tr>
- <tr>
-    <td> No. KP Calon </td>
-    <td>:</td>
-    <td> <input type="text" name="nokp" disabled value= '<?php echo $nokp; ?>'> </td>
-  </tr>
-  <tr>
-    <td> Nama Calon </td>
-    <td>:</td>
-    <td> <input type="text" name="namacalon" value= '<?php echo $namacalon; ?>' size="50"> </td>
-  </tr>
-  <tr>
-    <td>Kelas</td>
-    <td>:</td>
-    <td>
-      <select name="kelas" id="kelas">
-        
-        <option value="Pilih Kelas" <?php if ($kelas == ''){echo "selected";} ?>>PILIH KELAS</option>
-        <?php
-        while ($row2 =  mysqli_fetch_array($result2)){
-          $kelas2 = $row2['kelas'];
-          ?>
-          <option value="<?php echo $kelas2; ?>" <?php if ($kelas == $kelas2){echo "selected";} ?> ><?php echo $kelas2; ?></option>
+  <input type="hidden" name="idcalon" value='<?php echo $idcalon; ?>' >
+  <table class="table5">
+    <tr>
+      <th>Gambar</th>
+      <td class="td2">
+        <img src = '<?php echo $gambar; ?>' width='200px'><br>
+        <a href='confirm.php? idcalon=<?php echo $idcalon; ?>'> Kemaskini gambar </a> </i>
+      </td>
+    </tr>
+    <tr>
+      <th> Id Calon </th>
+      <td> <input type="text" name="idcalon" disabled value= '<?php echo $idcalon; ?>'> </td>
+    </tr>
+    <tr>
+      <th> No. KP Calon </th>
+      <td> <input type="text" name="nokp" disabled value= '<?php echo $nokp; ?>'> </td>
+    </tr>
+    <tr>
+      <th> Nama Calon </th>
+      <td> <input type="text" name="namacalon" value= '<?php echo $namacalon; ?>' size="50"> </td>
+    </tr>
+    <tr>
+      <th>Kelas</th>
+      <td>
+        <select name="kelas" id="kelas">
+          <option value="Pilih Kelas" <?php if ($kelas == ''){echo "selected";} ?>>PILIH KELAS</option>
           <?php
-        }
-        ?>
-      </select>
-    </td>
-  </tr>
-  <tr>
-    <td> IDadmin </td>
-    <td>:</td>
-    <td> <input type="text" name="idadmin" disabled value= '<?php echo $idadmin; ?>'> </td>
-  </tr>
-  <tr>
-
-<td> <input type="hidden" name="idcalon" value='<?php echo $idcalon; ?>'>
-
-
+          while ($row2 =  mysqli_fetch_array($result2)){
+            $kelas2 = $row2['kelas'];
+            ?>
+            <option value="<?php echo $kelas2; ?>" <?php if ($kelas == $kelas2){echo "selected";} ?> ><?php echo $kelas2; ?></option>
+            <?php
+          }
+          ?>
+        </select>
+      </td>
+    </tr>
+    <tr>
+      <th>Dicalonkan Oleh</th>
+      <td> <input type="text" name="idadmin" disabled value= '<?php echo $idadmin; ?>'> </td>
+    </tr>
+    <tr>
+      
+      <td class="td2" colspan='2'>
+        <button type="submit" name="submit" value="KEMASKINI">KEMASKINI</button>
+      </td>
+    </tr>
+  </table>
+</form>
+<a href="paparsemuacalon.php">Kembali</a>
+</center>
  
-    <br><br>
-
-
-    <button type="SUBMIT" name="submit" value="KEMASKINI">KEMASKINI</button></td>
-<br>
-    <a href="paparsemuacalon.php">Kembali</a>
- </td>
- </tr>
- </tr>
- </tr>
- </tr>
- </tr>
- </table>
- </form>
- </center>
- <?php
- while ($row2 = mysqli_fetch_array($result2)){
-          $kelas2 = $row2['kelas'];
-          echo $kelas2;
-        }
-  
-  echo $numRow;
-  ?>
+<?php
+include("footer.php");
+?>
--- confiq.php
+++ confiq.php
@@ -3,6 +3,6 @@ session_start();
 //sambungan mysqli dengan $con
 $con=mysqli_connect("localhost","root","","dbundi");
 if(mysqli_connect_errno()){
-echo "gagal sambungan pangkalan data mysql:".mysqli_connect_error();
+    echo "gagal sambungan pangkalan data mysql:".mysqli_connect_error();
 }
 ?>
