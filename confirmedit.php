e1dc89b9fe25a34800acd2fbc65871e4e0394d11
quickblack1 <najmeedaud@gmail.com>
2026-08-15 13:34:50 +0800

    Update confirmedit.php

--- confirmedit.php
+++ confirmedit.php
@@ -1,31 +1,29 @@
 <?php
 require('confiq.php');
 include("menuadmin.php");
-?>
-
-    <?php
 
 if(isset($_POST['Submit']))
 { 
+    $filepath = "gambarcalon/" . $_FILES["file"]["name"];
 
+    if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
+    {
+        echo "<center><img src=".$filepath." height=200 width=200></center>";
+    } 
+    else 
+    {
+        echo "<center>Error !!</center>";
+    }
+    
+    $idcalon = $_POST['idcalon'];
 
-$filepath = "images/" . $_FILES["file"]["name"];
-
-if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
-{
-echo "<center><img src=".$filepath." height=200 width=200></center>";
-} 
-else 
-{
-echo "<center>Error !!</center>";
-}
-$idcalon = $_POST['idcalon'];
-
-$query = "UPDATE calon SET gambar='$filepath' WHERE idcalon = '$idcalon'";
-$rekod=mysqli_query($con,$query);
-if ($rekod) {
-	echo"<script>alert('kemaskini berjaya'); window.location.href='paparcalon.php'; </script>";
-}
+    $query = "UPDATE calon SET gambar='$filepath' WHERE idcalon = '$idcalon'";
+    $rekod=mysqli_query($con,$query);
+    if ($rekod) {
+        ?>
+        <script>alert('kemaskini berjaya'); window.location.href='paparsemuacalon.php'; </script>
+        <?php
+    }
 }
 
 ?>
